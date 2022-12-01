f7c5207823e0df5db614632db01970995a95c925
Nicholaskapaz <nicholaslk@al.insper.edu.br>
2022-12-01 11:29:16 -0300

    classe_maciel

--- final/classes.py
+++ final/classes.py
@@ -1,4 +1,7 @@
 import pygame
+
+# Implementando as classes, da pedra, personagem_maciel
+
 class pedra(pygame.sprite.Sprite):
     def _init_(self,groups,assets):
         pygame.sprite.Sprite_init_(self)
@@ -8,3 +11,20 @@ class pedra(pygame.sprite.Sprite):
         self.rect = self.image.rect()
         self.rect.x = x
         self.rect.y = y
+
+class Personagem_Maciel(pygame.sprite.Sprite):
+
+
+    def __init__(self,groups,assets):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = assets['personagem_maciel']
+        self.maciel_padrao = True
+        self.maciel_pular = False
+        self.step_index = 0 
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+        self.groups = groups
+        self.assets = assets
+        self.speedx = 0
+        self.speedy = 0 
